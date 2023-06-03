b8e3c181c44d53511ed9691217940664d38ff13c
zik <81015305+zik000001@users.noreply.github.com>
2023-06-03 18:23:47 +0800

    Update clean-blog.js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1056,10 +1056,3 @@ jQuery(document).ready(function($) {
             });
     }
 });
-
-const dp = new DPlayer({
-    container: document.getElementById('dplayer'),
-    video: {
-        url: 'demo.mp4',
-    },
-});
